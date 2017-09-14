2ff3b648515a68cbdfdddbc0b498d1230e4b8f4f
Rhonda Lewis <rhonda.earl@gmail.com>
2017-09-14 16:16:52 -0400

    Add component to pop up work examples in a modal

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -26,17 +26,26 @@ describe("ExampleWork component", () => {
 
   it("Should be a 'section' element", () => {
     console.log(component.debug());
-    expect(component.type()).toEqual('section');
+    expect(component.type()).toEqual('span');
   });
 
   it("Should contain as many children as there are work examples", () => {
     expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
   });
 
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+
+  });
+
 });
 
 describe("ExampleWorkBubble component", () => {
-  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+  let mockOpenModalFn = jest.fn();
+  let component = shallow(<ExampleWorkBubble example={myWork[1]} openModal={mockOpenModalFn}/>);
 
   let images = component.find("img");
 
@@ -46,6 +55,11 @@ describe("ExampleWorkBubble component", () => {
 
   it("Should have the image src set correctly", () => {
     expect(images.node.props.src).toEqual(myWork[1].image.src);
+  });
+
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
   });
 
 });
